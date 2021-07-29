08187e2d57d414e78605fd707666880049dadf4e
stormbringer88 <alessandronessi@msn.com>
2021-07-29 17:17:34 +0200

    changed input mode

--- script/script.js
+++ script/script.js
@@ -1,18 +1,24 @@
-var userAge = parseFloat(prompt("Hello, insert your age:"));
-if((userAge > -1 && userAge > 160) || Number.isNaN(userAge)==true){
-    alert("the user age you insered is not correct");
-} else{
-    var travelDistance = parseFloat(prompt("Insert your destination's distance:"));
-    if(travelDistance < 0 || Number.isNaN(travelDistance)==true)
-    {
-        alert("the distance you insered is not correct");
-    }else{
-        var tiketPrice = travelDistance * 0.21; 
-        if(userAge < 18){
-            tiketPrice *= 0.8;
-        }else if(userAge > 65){
-            tiketPrice *= 0.6;
+function calculatePrice(){
+    var userAge = parseFloat(document.getElementById("userAge").value);
+    if((userAge > -1 && userAge > 160) || Number.isNaN(userAge)==true){
+        alert("the user age you insered is not correct");
+    } else{
+        var travelDistance = parseFloat(document.getElementById("travelDistance").value);
+        if(travelDistance < 0 || Number.isNaN(travelDistance)==true)
+        {
+            alert("the distance you insered is not correct");
+        }else{
+            var tiketPrice = travelDistance * 0.21; 
+            if(userAge < 18){
+                tiketPrice *= 0.8;
+            }else if(userAge > 65){
+                tiketPrice *= 0.6;
+            }
+            document.getElementById("result").value = parseInt(tiketPrice); 
+            // alert(parseInt((tiketPrice-parseInt(tiketPrice))*100));
+            document.getElementById("result__module").innerHTML = "." + parseInt((tiketPrice-parseInt(tiketPrice))*100);
         }
-        document.getElementById("result").value = tiketPrice.toFixed(2); 
     }
 }
+
+
